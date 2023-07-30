5ea01b2b8afddffa1b81c742d1b8c95aa5e2d462
Ikennaf1 <iiikbros202@gmail.com>
2023-07-30 16:24:58 +0100

    Posts route done

---
+++ app/Http/Controllers/ExportController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Export;
+use App\Http\Requests\StoreExportRequest;
+use App\Http\Requests\UpdateExportRequest;
+
+class ExportController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreExportRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Export $export)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Export $export)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateExportRequest $request, Export $export)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Export $export)
+    {
+        //
+    }
+}
