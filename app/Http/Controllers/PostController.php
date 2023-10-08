c02fc616d923e8b4e027feecc8e55cc9e83347b0
Ikennaf1 <iiikbros202@gmail.com>
2023-10-09 00:43:06 +0100

    Updates the post controller

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Requests\StorePostRequest;
 use App\Http\Requests\UpdatePostRequest;
@@ -133,6 +134,7 @@ class PostController extends Controller
     public function show(Post $post)
     {
         // return Post::find($post);
+        $post->author = User::find($post->user_id)->name;
         return view('front.post', [
             'post' => $post
         ]);
