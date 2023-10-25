b5073bff0def56e643336637b6369d87d8757711
Ikennaf1 <iiikbros202@gmail.com>
2023-10-25 01:45:40 +0100

    Images can now be uploaded directly in post body

--- resources/views/dashboard/posts/create.blade.php
+++ resources/views/dashboard/posts/create.blade.php
@@ -6,8 +6,33 @@
         statusbar: false,
         // toolbar: 'alignleft aligncenter alignright',
         automatic_uploads: true,
+        document_base_url: 'http://localhost:8000/',
+        relative_urls: false,
+        remove_script_host: true,
         images_upload_url: '/uploads/images/postImageAcceptor',
-        images_reuse_filename: true
+        images_upload_base_path: '/uploads',
+        images_reuse_filename: true,
+        file_picker_types: 'image',
+        file_picker_callback: function(cb, value, meta) {
+            var input = document.createElement('input');
+            input.setAttribute('type', 'file');
+            input.setAttribute('accept', 'image/*');
+            input.onchange = function() {
+                var file = this.files[0];
+
+                var reader = new FileReader();
+                reader.readAsDataURL(file);
+                reader.onload = function () {
+                    var id = 'blobid' + (new Date()).getTime();
+                    var blobCache =  tinymce.activeEditor.editorUpload.blobCache;
+                    var base64 = reader.result.split(',')[1];
+                    var blobInfo = blobCache.create(id, file, base64);
+                    blobCache.add(blobInfo);
+                    cb(blobInfo.blobUri(), { title: file.name });
+                };
+            };
+            input.click();
+        }
     });
 </script>
 <div class="flex flex-col gap-8">
